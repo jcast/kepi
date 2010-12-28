d72086de238927b4eea3900f830c71e69e496423
jcastagna <jeremie.castagna@gmail.com>
2010-12-28 14:39:59 -0800

    added in-path param processing

--- lib/kepi/api.rb
+++ lib/kepi/api.rb
@@ -22,12 +22,12 @@ class Kepi
     # If block is given, passes it the newly created Endpoint instance.
 
     def self.endpoint http_method, path
-      http_method = http_method.to_s.upcase
-      matcher     = matcher_for path
+      http_method   = http_method.to_s.upcase
+      matcher, keys = matcher_for path
 
       new_endpoint = Endpoint.new
 
-      self.endpoints[http_method][matcher] = new_endpoint
+      self.endpoints[http_method][matcher] = [new_endpoint, keys]
 
       yield new_endpoint if block_given?
     end
@@ -51,11 +51,12 @@ class Kepi
           when *special_chars
             Regexp.escape(match)
           else
+            keys << $2[1..-1]
             "([^/?#]+)"
           end
         end
 
-      /^#{pattern}$/
+      [/^#{pattern}$/, keys]
     end
 
 
@@ -94,7 +95,9 @@ class Kepi
     # Call the validation stack.
 
     def call env
-      validate env['PATH_INFO'], env['params']
+      req = Rack::Request.new env
+
+      validate req.path_info, req.params
       when_valid @app, env
 
     rescue EndpointUndefined => err
@@ -107,13 +110,44 @@ class Kepi
 
     ##
     # Find the first endpoint that matches the given path.
+    # Returns an array containing the matcher, endpoint, and keys
+    # Keys are used to process params in the path.
 
     def find_endpoint http_method, path
-      self.class.endpoints[http_method].each do |matcher, endpoint|
-        return endpoint if path =~ matcher
+      self.class.endpoints[http_method].each do |matcher, (endpoint, keys)|
+        return [matcher, endpoint, keys] if path =~ matcher
       end
 
       nil
+    end
+
+
+    ##
+    # Process request path and return the matching params.
+
+    def process_path_params path, pattern, keys
+      match = pattern.match path
+      return Hash.new unless match && !keys.empty?
+
+      values = match.captures.to_a
+
+      if keys.any?
+        keys.zip(values).inject({}) do |hash,(k,v)|
+          if k == 'splat'
+            (hash[k] ||= []) << v
+          else
+            hash[k] = v
+          end
+
+          hash
+        end
+
+      elsif values.any?
+        {'captures' => values}
+
+      else
+        {}
+      end
     end
 
 
@@ -126,10 +160,15 @@ class Kepi
     #   Kepi::Endpoint::ParamUndefined - only if endpoint is strict with params
 
     def validate path, params
-      endpoint = find_endpoint path
-      raise EndpointUndefined, path unless endpoint || @passthrough
+      pattern, endpoint, keys = find_endpoint path
 
-      endpoint.validate params
+      if endpoint
+        params = params.merge process_path_params(path, pattern, keys)
+        endpoint.validate params
+
+      elsif !@passthrough
+        raise EndpointUndefined, path
+      end
     end
 
 
